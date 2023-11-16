f37249b6d32fa0bc96c85c597bb69aab83235e56
Ken Christian Saar <ken-christian.saar@voco.ee>
2023-11-16 11:22:50 +0200

    practice4 - v6rdlus peab olema sisestatud jutum2rkidega, sest muidu see ei t33ta korrektselt

---
+++ Basics/practice4.ps1
@@ -0,0 +1,13 @@
+﻿$number1 = Read-Host "Enter the first value"
+$number2 = Read-Host "Enter the second value"
+
+# Miskip2rast v6rdlus ei t33ta, kui all olevad muutujad pole jutum2rkides.
+
+if("$number1" -gt "$number2")
+    {
+    Write-Host "The higher number is: "$number1
+    }
+else
+    {
+    Write-Host "The higher number is: "$number2
+    }
