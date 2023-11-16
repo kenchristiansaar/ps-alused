579dc3a1eab61c268f3d6b5cf96122f7db2d5a7f
Ken Christian Saar <ken-christian.saar@voco.ee>
2023-11-16 11:33:30 +0200

    practice4 kysimus 2

---
+++ Basics/practice4-2.ps1
@@ -0,0 +1,23 @@
+﻿Write-Host "Select country: " -ForegroundColor Yellow
+Write-Host "1 : India" -ForegroundColor Cyan
+Write-Host "2 : Australia" -ForegroundColor Cyan
+Write-Host "3 : China" -ForegroundColor Cyan
+
+$country = Read-Host "Please select a country"
+
+if("$country" -eq 1)
+    {
+    Write-Host "India's capital is New Delhi!" -ForegroundColor Green
+    }
+elseif("$country" -eq "2")
+    {
+    Write-Host "Australia's capital is Canberra!" -ForegroundColor Green
+    }
+elseif("$country" -eq "3")
+    {
+    Write-Host "China's capital is Beijing!" -ForegroundColor Green
+    }
+else
+    {
+    Write-Host "Please select a country from the list provided." -ForegroundColor Red
+    }
