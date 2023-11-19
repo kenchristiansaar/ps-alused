5531a8eed18667c9844c9e5818f73d4a8363c800
Ken Christian Saar <ken-christian.saar@voco.ee>
2023-11-19 15:42:36 +0200

    practice5-2

---
+++ Basics/practice5-2.ps1
@@ -0,0 +1,10 @@
+﻿Get-ChildItem -Path "C:\Temp\Test"
+
+$file = Get-ChildItem -Path "C:\Temp\Test" | where {$_.Name -like "*.csv"} | Select Name,Length
+
+$sizeKB = $file.Length/1KB
+$sizeMB = $file.Length/1MB
+
+Write-Host "`nFile Name: "$file.Name
+Write-Host "Size in KB: "$sizeKB
+Write-Host "Size in MB: "$sizeMB
