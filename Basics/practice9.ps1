a37d4662395d30d68a2d2f081f3ce4c8744ccb91
Ken Christian Saar <ken-christian.saar@voco.ee>
2023-11-19 16:19:14 +0200

    practice9

---
+++ Basics/practice9.ps1
@@ -0,0 +1,19 @@
+﻿$ctl = 0
+
+do
+{
+    $ch = $null
+    $ch = Get-Process | ?{$_.name -like "note*"}
+        if($ch -ne $null)
+            {$ct2 = 2
+                Write-Host "Notepad is running"
+                Start-Sleep -Seconds 1
+                $ctl++
+             }
+        else
+            {$ct2 = 1}
+}
+
+while($ct2 -ne 1)
+
+$ctl
