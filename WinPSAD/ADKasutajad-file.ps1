ef2263f3d4efc39f2adc72ee6499b7eb0d94ac20
unknown <Administrator@sv-kool.local>
2023-11-30 01:26:07 -0800

    ADKasutajad failist

---
+++ WinPSAD/ADKasutajad-file.ps1
@@ -0,0 +1,13 @@
+﻿# Adusers.csv asukoht
+
+$file = "C:\Users\Administrator\Desktop\adusers.csv"
+
+# Importin faili
+
+$users = Import-Csv $file -Encoding Default -Delimiter ";"
+
+# Foreachiga uued read iga kasutaja parameetri jaoks
+
+foreach ($user in $users){
+    echo $user
+    }
