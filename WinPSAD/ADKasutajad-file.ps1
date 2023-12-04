966318b0417631600ad20b04e36007eacab4bdf8
unknown <Administrator@sv-kool.local>
2023-12-04 01:04:52 -0800

    Lisasin t2pit2htede muutmise skripti

--- WinPSAD/ADKasutajad-file.ps1
+++ WinPSAD/ADKasutajad-file.ps1
@@ -8,11 +8,47 @@ $users = Import-Csv $file -Encoding Default -Delimiter ";"
 
 # Foreachiga uued read iga kasutaja parameetri jaoks
 
+# Funktsioon muudab t2pit2hed tava ladina t2hestikuks
+
+function Translit {
+
+    param(
+        [string] $inputString
+        )
+
+        # M22rab karakterid, mida t6lkida
+
+        $Translit = @{
+        [char]'ä' = "a"
+        [char]'ö' = "o"
+        [char]'ü' = "u"
+        [char]'õ' = "o"
+        }
+
+        $outputString=""
+
+        foreach ($character in $inputCharacter = $inputString.ToCharArray())
+        {
+            if ($Translit[$character] -cne $Null)
+                {
+                    $outputString += $Translit[$character]
+                }
+
+            else
+                {
+                    $outputString += $character
+                }
+        }
+        Write-Output $outputString
+    }
+                        
+
 foreach ($user in $users){
 
     # kasutaja nimi on eesnimi.perenimi
 
     $username = $user.FirstName + "." + $user.LastName
     $username = $username.ToLower()
+    $username = Translit($username)
     echo $username
     }
