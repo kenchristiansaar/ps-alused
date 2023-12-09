8a8ec2aae112d7822c5f71de3f9d965a01813f20
Ken Christian Saar <ken-christian.saar@voco.ee>
2023-12-09 14:53:59 +0200

    yl1 - Kasutaja loomine tema ees- ja perenime j2rgi

---
+++ Haldus/kasutaja_loomine_yl1.ps1
@@ -0,0 +1,26 @@
+﻿$EesNimi = Read-Host "Sisesta oma eesinimi"
+$PereNimi = Read-Host "Sisesta oma perenimi"
+
+$EesNimiMadal = $EesNimi.ToLower()
+$PereNimiMadal = $PereNimi.ToLower()
+
+$EesNimiKorge = ( Get-Culture ).TextInfo.ToTitleCase( $EesNimi.ToLower() )
+$PereNimiKorge = ( Get-Culture ).TextInfo.ToTitleCase( $PereNimi.ToLower() )
+$TaisNimi = "$EesNimiKorge" + " " + "$PereNimiKorge"
+
+$KontoKirjeldus = "Lokaalne konto"
+
+$KasutajaNimi = "$EesNimiMadal.$PereNimiMadal"
+
+$Parool = "Parool1!" | ConvertTo-SecureString -AsPlainText -Force
+
+try
+{
+    New-LocalUser -Name $KasutajaNimi -Password $Parool -FullName $TaisNimi -Description "$KontoKirjeldus" -ErrorAction Stop
+    Write-Host "Loodav kasutaja on $KasutajaNimi"
+}
+catch
+{
+    Write-Host "Kasutaja loomisega tekkis probleem"
+    Write-Host "Probleemi detailid: $?"
+}
