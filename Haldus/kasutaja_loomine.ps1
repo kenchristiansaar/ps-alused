5f50d59f9287b32ad8fff3bceb22dc142f9ffb8e
Ken Christian Saar <ken-christian.saar@voco.ee>
2023-12-09 14:05:18 +0200

    Kasutaja loomine parameetritega

--- Haldus/kasutaja_loomine.ps1
+++ Haldus/kasutaja_loomine.ps1
@@ -1,3 +1,11 @@
-﻿$KasutajaParool = ConvertTo-SecureString "qwerty" -AsPlainText -Force
-
-New-LocalUser "kasutaja1" -Password $KasutajaParool -FullName "Esimene kasutaja" -Description "Local Account - kasutaja1"
+﻿if($args.Count -ne 3 ){
+    echo ""
+    echo "Sisestage järgmised parameetrid tühikutega: Kasutajanimi Ees_Perenimi Konto_kirjeldus"
+}
+else {
+    $KasutajaNimi = $args[0]
+    $TaisNimi = $args[1]
+    $Kontokirjeldus = $args[2]
+    $KasutajaParool = ConvertTo-SecureString "qwerty" -AsPlainText -Force
+    New-LocalUser "$KasutajaNimi" -Password $KasutajaParool -FullName "$TaisNimi" -Description "$Kontokirjeldus"
+    }
