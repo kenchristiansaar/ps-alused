4af3c6aca7549745a54b32c962e1491bf635ef5e
Ken Christian Saar <ken-christian.saar@voco.ee>
2023-12-09 14:20:20 +0200

    Kasutajate loomine faili abil

--- Haldus/kasutaja_loomine.ps1
+++ Haldus/kasutaja_loomine.ps1
@@ -1,11 +1,15 @@
-﻿if($args.Count -ne 3 ){
-    echo ""
-    echo "Sisestage järgmised parameetrid tühikutega: Kasutajanimi Ees_Perenimi Konto_kirjeldus"
-}
-else {
-    $KasutajaNimi = $args[0]
-    $TaisNimi = $args[1]
-    $Kontokirjeldus = $args[2]
-    $KasutajaParool = ConvertTo-SecureString "qwerty" -AsPlainText -Force
-    New-LocalUser "$KasutajaNimi" -Password $KasutajaParool -FullName "$TaisNimi" -Description "$Kontokirjeldus"
+﻿# Faili asukoht
+
+$Fail = "C:\Users\User\ps-alused\Haldus\kasutajad.csv"
+
+# Faili sisu lugemine
+
+$Kasutajad = Import-Csv $Fail -Encoding Default -Delimiter ";"
+
+foreach ($kasutaja in $Kasutajad){
+    $KasutajaNimi = $kasutaja.KasutajaNimi
+    $Taisnimi = $kasutaja.TaisNimi
+    $KontoKirjeldus = $kasutaja.KontoKirjeldus
+    $Parool = $kasutaja.Parool | ConvertTo-SecureString -AsPlainText -Force
+    New-LocalUser -Name $KasutajaNimi -Password $Parool -FullName $TaisNimi -Description "$KontoKirjeldus"
     }
