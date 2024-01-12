a2343a03499835e75a1dd7ed3b6d7144c99c14a0
Ken Christian Saar <ken-christian.saar@voco.ee>
2024-01-12 15:50:05 +0200

    Muutsin yl1 skripti, eemaldasin read, mida pole enam vaja, muutsin loomise kustutamiseks.

---
+++ Haldus/kasutaja_loomine_yl2.ps1
@@ -0,0 +1,18 @@
+﻿$EesNimi = Read-Host "Sisesta oma eesinimi"
+$PereNimi = Read-Host "Sisesta oma perenimi"
+
+$EesNimiMadal = $EesNimi.ToLower()
+$PereNimiMadal = $PereNimi.ToLower()
+
+$KasutajaNimi = "$EesNimiMadal.$PereNimiMadal"
+
+try
+{
+    Remove-LocalUser -Name $KasutajaNimi -ErrorAction Stop | Out-Null
+    Write-Host "Kustutatav kasutaja on $KasutajaNimi"
+}
+catch
+{
+    Write-Host "Kasutaja kustutamisega tekkis probleem."
+    Write-Host "Probleemi detailid: $_"
+}
